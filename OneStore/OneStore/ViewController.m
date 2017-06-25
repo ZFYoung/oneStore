3135c2f14735f34b20a76de2011765524b8fc1ff
yecc <yecc@gmail.com>
2017-06-25 22:10:07 +0800

    init

--- OneStore/OneStore/ViewController.m
+++ OneStore/OneStore/ViewController.m
@@ -9,7 +9,7 @@
 #import "ViewController.h"
 #import <WebKit/WebKit.h>
 
-@interface ViewController ()<WKUIDelegate>
+@interface ViewController ()<WKScriptMessageHandler, WKNavigationDelegate, WKUIDelegate,UINavigationControllerDelegate,UIImagePickerControllerDelegate>
 @property (nonatomic, strong) WKWebView *webView;
 @end
 
@@ -17,25 +17,215 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    [self.webView loadRequest:[NSURLRequest requestWithURL:[NSURL URLWithString:@"http://www.xfx8888.com"]]];
+    NSURL *url = [NSURL URLWithString:@"http://www.xfx8888.com"];
+    NSURLRequest *request = [NSURLRequest requestWithURL:url];
+    [self.webView loadRequest:request];
     // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (BOOL)prefersStatusBarHidden
+{
+    return YES;
 }
 
 - (WKWebView *)webView
 {
     if (_webView == nil) {
-        _webView = [[WKWebView alloc] init];
+        CGRect frame = CGRectMake(0, 0, CGRectGetWidth(self.view.frame), CGRectGetHeight(self.view.frame));
+        WKWebViewConfiguration *config = [WKWebViewConfiguration new];
+        config.preferences = [WKPreferences new];;
+        config.preferences.minimumFontSize = 10;
+        config.preferences.javaScriptEnabled = YES;
+        config.preferences.javaScriptCanOpenWindowsAutomatically = NO;
+        
+        config.processPool = [[WKProcessPool alloc] init];
+        
+        // 通过JS与webview内容交互
+        config.userContentController = [[WKUserContentController alloc] init];
+        // 注入JS对象名称AppModel，当JS通过AppModel来调用时，
+        // 我们可以在WKScriptMessageHandler代理中接收到
+        [config.userContentController addScriptMessageHandler:self name:@"iosModel"];
+
+        _webView = [[WKWebView alloc] initWithFrame:frame configuration:config];
+        _webView.navigationDelegate = self;
         _webView.UIDelegate = self;
-        _webView.frame = CGRectMake(0, 20, CGRectGetWidth(self.view.frame), CGRectGetHeight(self.view.frame)-20);
         [self.view addSubview:_webView];
     }
     return _webView;
 }
 
-
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+#pragma mark - WKScriptMessageHandler
+// 通过这个方法获取 JS传来的json字符串
+- (void)userContentController:(WKUserContentController *)userContentController
+      didReceiveScriptMessage:(WKScriptMessage *)message
+{
+    if ([message.name isEqualToString:@"iosModel"]) {
+        // 打印所传过来的参数，只支持NSNumber, NSString, NSDate, NSArray,
+        // NSDictionary, and NSNull类型
+        NSLog(@"JS传来的json字符串 ：  %@", message.body);
+        NSDictionary *jsDictionary = message.body;
+        if ([jsDictionary[@"means"] isEqualToString:@"获取系统图片"])
+        {
+            
+        }
+    }
+}
+
+
+#pragma mark - WKNavigationDelegate
+// 请求开始前，会先调用此代理方法
+// 与UIWebView的
+// - (BOOL)webView:(UIWebView *)webView
+// shouldStartLoadWithRequest:(NSURLRequest *)request
+// navigationType:(UIWebViewNavigationType)navigationType;
+// 类型，在请求先判断能不能跳转（请求）
+
+- (void)webView:(WKWebView *)webView decidePolicyForNavigationAction:(WKNavigationAction *)navigationAction decisionHandler:(void (^)(WKNavigationActionPolicy))decisionHandler {
+    
+    NSString *hostname = navigationAction.request.URL.host.lowercaseString;
+    if (navigationAction.navigationType == WKNavigationTypeLinkActivated
+        && ![hostname containsString:@".xfx8888.com"]) {
+        // 对于跨域，需要手动跳转， 用系统浏览器（Safari）打开
+        [[UIApplication sharedApplication] openURL:navigationAction.request.URL];
+        
+        // 不允许web内跳转
+        decisionHandler(WKNavigationActionPolicyCancel);
+    } else {
+        decisionHandler(WKNavigationActionPolicyAllow);
+    }
+    
+}
+
+
+// 在响应完成时，会回调此方法
+// 如果设置为不允许响应，web内容就不会传过来
+- (void)webView:(WKWebView *)webView
+decidePolicyForNavigationResponse:(WKNavigationResponse *)navigationResponse
+decisionHandler:(void (^)(WKNavigationResponsePolicy))decisionHandler {
+    decisionHandler(WKNavigationResponsePolicyAllow);
+    
+}
+
+
+// 开始导航跳转时会回调
+- (void)webView:(WKWebView *)webView didStartProvisionalNavigation:(null_unspecified WKNavigation *)navigation {
+    
+}
+
+// 接收到重定向时会回调
+- (void)webView:(WKWebView *)webView
+didReceiveServerRedirectForProvisionalNavigation:(null_unspecified WKNavigation *)navigation {
+    
+}
+
+
+// 导航失败时会回调
+- (void)webView:(WKWebView *)webView
+didFailProvisionalNavigation:(null_unspecified WKNavigation *)navigation withError:(NSError *)error {
+    
+}
+
+
+// 页面内容到达main frame时回调
+- (void)webView:(WKWebView *)webView didCommitNavigation:(null_unspecified WKNavigation *)navigation {
+    
+}
+
+// 导航完成时，会回调（也就是页面载入完成了）
+- (void)webView:(WKWebView *)webView didFinishNavigation:(null_unspecified WKNavigation *)navigation {
+    NSLog(@"66===%s", __FUNCTION__);
+    // 禁用选中效果
+    [self.webView evaluateJavaScript:@"document.documentElement.style.webkitUserSelect='none'" completionHandler:nil];
+    [self.webView evaluateJavaScript:@"document.documentElement.style.webkitTouchCallout='none'" completionHandler:nil];
+}
+// 导航失败时会回调
+- (void)webView:(WKWebView *)webView didFailNavigation:
+(null_unspecified WKNavigation *)navigation withError:(NSError *)error
+{
+    
+}
+
+
+/* 对于HTTPS的都会触发此代理，如果不要求验证，传默认就行
+ 如果需要证书验证，与使用AFN进行HTTPS证书验证是一样的 */
+
+- (void)webView:(WKWebView *)webView didReceiveAuthenticationChallenge:
+(NSURLAuthenticationChallenge *)challenge completionHandler:
+(void (^)(NSURLSessionAuthChallengeDisposition disposition,
+          NSURLCredential *__nullable credential))completionHandler
+{
+    completionHandler(NSURLSessionAuthChallengePerformDefaultHandling, nil);
+}
+
+
+// 9.0才能使用，web内容处理中断时会触发
+/*
+ - (void)webViewWebContentProcessDidTerminate:(WKWebView *)webView {
+ }
+ */
+#pragma mark - WKUIDelegate
+- (void)webViewDidClose:(WKWebView *)webView {
+    
+}
+/* 在JS端调用alert函数时，会触发此代理方法。JS端调用alert时所传的数据可以通过message拿到 在原生得到结果后，需要回调JS，是通过completionHandler回调 */
+- (void)webView:(WKWebView *)webView runJavaScriptAlertPanelWithMessage:(NSString *)message
+initiatedByFrame:(WKFrameInfo *)frame completionHandler:(void (^)(void))completionHandler
+{
+    UIAlertController *alert = [UIAlertController alertControllerWithTitle:@"alert" message:message preferredStyle:UIAlertControllerStyleAlert];
+    [alert addAction:[UIAlertAction actionWithTitle:@"确定" style:UIAlertActionStyleDefault handler:^(UIAlertAction * _Nonnull action) {
+        completionHandler();
+    }]];
+    
+    [self presentViewController:alert animated:YES completion:NULL];
+    NSLog(@"%@", message);
+    
+}
+
+// JS端调用confirm函数时，会触发此方法
+// 通过message可以拿到JS端所传的数据
+// 在iOS端显示原生alert得到YES/NO后
+// 通过completionHandler回调给JS端
+- (void)webView:(WKWebView *)webView
+runJavaScriptConfirmPanelWithMessage:(NSString *)message
+initiatedByFrame:(WKFrameInfo *)frame
+completionHandler:(void (^)(BOOL result))completionHandler {
+    UIAlertController *alert = [UIAlertController alertControllerWithTitle:@"confirm" message:message preferredStyle:UIAlertControllerStyleAlert];
+    [alert addAction:[UIAlertAction actionWithTitle:@"确定"
+                                              style:UIAlertActionStyleDefault handler:^(UIAlertAction * _Nonnull action){
+                                                  completionHandler(YES);
+                                              }]];
+    [alert addAction:[UIAlertAction actionWithTitle:@"取消"
+                                              style:UIAlertActionStyleCancel handler:^(UIAlertAction * _Nonnull action)
+                      {
+                          completionHandler(NO);
+                      }]];
+    [self presentViewController:alert animated:YES completion:NULL];
+    NSLog(@"%@", message);
+}
+// JS端调用prompt函数时，会触发此方法
+// 要求输入一段文本
+// 在原生输入得到文本内容后，通过completionHandler回调给JS
+- (void)webView:(WKWebView *)webView
+runJavaScriptTextInputPanelWithPrompt:(NSString *)prompt
+    defaultText:(nullable NSString *)defaultText
+initiatedByFrame:(WKFrameInfo *)frame
+completionHandler:(void (^)(NSString * __nullable result))completionHandler
+{
+    UIAlertController *alert = [UIAlertController alertControllerWithTitle:prompt message:defaultText preferredStyle:UIAlertControllerStyleAlert];
+    [alert addTextFieldWithConfigurationHandler:^(UITextField * _Nonnull textField) {
+        textField.textColor = [UIColor redColor];
+    }];
+    [alert addAction:[UIAlertAction actionWithTitle:@"确定"
+                                              style:UIAlertActionStyleDefault handler:^(UIAlertAction * _Nonnull action) {  
+                                                  completionHandler([[alert.textFields lastObject] text]);  
+                                              }]];  
+    
+    [self presentViewController:alert animated:YES completion:NULL];  
 }
 
 
